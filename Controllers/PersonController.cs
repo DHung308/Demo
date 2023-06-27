7409850b0a52fa2be40490aedd72e15755f5a824
DHung308 <nguyenduonghung30082001@gmail.com>
2023-06-27 09:32:08 +0700

    demo dau tien

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Demo.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.greeting = "Xin chào";
+            ViewData["greeting2"] = "Tôi đang sử dụng view data";
+            TempData["greeting3"] = " Nó là dữ liệu tạm thời";
+            return View();
+        }
+
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Domain/Person.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Andress { get; set; }
+
+    }
+}
